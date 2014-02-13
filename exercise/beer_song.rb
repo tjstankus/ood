2c824b47c7c9b7f90496141e2f16754dd99df0db
TJ Stankus <tj@stank.us>
2014-02-13 18:16:48 -0500

    delegate to a strategy object to make the first spec pass

--- exercise/beer_song.rb
+++ exercise/beer_song.rb
@@ -1,5 +1,20 @@
-class BeerSong
+class VerseStrategy
+
   def verse(num)
+    "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" +
+    "Take one down and pass it around, #{num-1} bottles of beer on the wall.\n"
+  end
+
+end
+
+class BeerSong
+
+  def initialize
+    @strategy = VerseStrategy.new
+  end
+
+  def verse(num)
+    @strategy.verse(num)
   end
 
   def verses(from, downto=0)
