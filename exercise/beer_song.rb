29059985c1495b7b8e199a620fc4ed8c5dae6ae1
TJ Stankus <tj@stank.us>
2014-02-12 21:57:32 -0500

    DRY up code

--- exercise/beer_song.rb
+++ exercise/beer_song.rb
@@ -10,11 +10,9 @@ class BeerSong
   private
 
   def verse_second_line(num)
-    if num == 1
-      "Take one down and pass it around, #{num} bottle of beer on the wall.\n"
-    else
-      "Take one down and pass it around, #{num} bottles of beer on the wall.\n"
-    end
+    phrase = "#{num} " + (num == 1 ? "bottle" : "bottles")
+    "Take one down and pass it around, #{phrase} of beer on the wall.\n"
   end
+
 
 end
