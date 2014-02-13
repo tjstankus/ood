5e98df56c4b4ba4d24381cbfdf2658d83f0dff75
TJ Stankus <tj@stank.us>
2014-02-12 21:44:27 -0500

    get first spec passing, make the rest pending

--- exercise/beer_song.rb
+++ exercise/beer_song.rb
@@ -1,5 +1,7 @@
 class BeerSong
   def verse(num)
+    "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" +
+    "Take one down and pass it around, #{num-1} bottles of beer on the wall.\n"
   end
 
   def verses(from, downto=0)
