127c87e00a119752e8f28d0b3a0340f9c54c4d42
TJ Stankus <tj@stank.us>
2014-02-12 22:12:49 -0500

    introduce meh refactoring
    As expected, I'm to the point that I hate this implementation. It's time
    to break out better refactorings.

--- exercise/beer_song.rb
+++ exercise/beer_song.rb
@@ -13,15 +13,16 @@ class BeerSong
   end
 
   def verse_second_line(num)
-    if num > 0
-      "Take one down and pass it around, #{phrase(num)} of beer on the wall.\n"
-    else
-      "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    end
+    pronoun = num > 0 ? "one" : "it"
+    "Take #{pronoun} down and pass it around, #{phrase(num)} of beer on the wall.\n"
   end
 
   def phrase(num)
-    "#{num} " + (num == 1 ? "bottle" : "bottles")
+    if num > 0
+      "#{num} " + (num == 1 ? "bottle" : "bottles")
+    else
+      "no more bottles"
+    end
   end
 
 end
