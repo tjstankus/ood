124099d4aa3b12735127c27081a2f094131ccb0a
TJ Stankus <tj@stank.us>
2014-02-13 19:00:33 -0500

    refactor strategy classes to use class methods
    I'm not sure I like this. I generally prefer instance methods to class
    methods. But in this case, it seems a little cleaner than the last
    commit.

--- exercise/beer_song.rb
+++ exercise/beer_song.rb
@@ -1,12 +1,12 @@
 class VerseStrategy
-  def verse(num)
+  def self.verse(num)
     "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" +
     "Take one down and pass it around, #{num-1} bottles of beer on the wall.\n"
   end
 end
 
 class VerseTwoStrategy
-  def verse(num)
+  def self.verse
     "2 bottles of beer on the wall, 2 bottles of beer.\n" +
     "Take one down and pass it around, 1 bottle of beer on the wall.\n"
   end
@@ -15,11 +15,11 @@ end
 class BeerSong
 
   VERSE_STRATEGIES = {
-    2 => VerseTwoStrategy
+    2 => VerseTwoStrategy.verse
   }
 
   def verse(num)
-    verse_strategy(num).verse(num)
+    verse_via_strategy(num)
   end
 
   def verses(from, downto=0)
@@ -27,8 +27,8 @@ class BeerSong
 
   private
 
-  def verse_strategy(num)
-    VERSE_STRATEGIES.fetch(num, VerseStrategy).new
+  def verse_via_strategy(num)
+    VERSE_STRATEGIES.fetch(num, VerseStrategy.verse(num))
   end
 
 end
